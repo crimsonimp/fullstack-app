bc56d3eea64a80e67084ad0f7861804ee2782a97
Volodymyr Bakay <volodymyr.bakay@gmail.com>
2020-05-25 10:46:14 +0300

    add schemas and resolvers. connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphqlSchema = require('./graphql/schema/index');
+const graphqlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -7,6 +12,26 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res, next) => res.send('Hello'));
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-xekfh.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
